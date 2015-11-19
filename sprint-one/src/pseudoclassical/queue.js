1a52afa8ad4c918e7767e3dd9b121930a9ebe14c
Egan Tamashiro <egantamashiro@gmail.com>
2015-11-18 23:20:32 -0800

    added size method to queue pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,10 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.queueSize = 0;
 };
 
-
+Queue.prototype.size = function(){
+  return this.queueSize >= 0 ? this.queueSize : 0;
+}
