bd06a56c8f7772c6776bd129509ef6f0e0a66a7f
jpanopio <jcpanopio1@gmail.com>
2015-11-20 11:46:07 -0800

    passes all hashTables tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,17 +7,39 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if(this._storage.get(index) === undefined) {
+    this._storage.set(index, [[k,v]]);
+  } else {
+    var bucket = this._storage.get(index);
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] === k) {
+        bucket[i][1] = v;
+      }
+    }
+    bucket.push([k, v]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(bucket[i] !== undefined && bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
+  var bucket = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(bucket[i][0] === k) {
+      bucket[i] = undefined;
+      break;
+    }
+  }
 };
 
 
