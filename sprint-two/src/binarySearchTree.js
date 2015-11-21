e7d7a2f12177f8e0c89f8d5c4b7241934cbce3c1
jpanopio <jcpanopio1@gmail.com>
2015-11-21 10:19:23 -0800

    Implement binarySearchTree's insert method

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,37 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(bstMethods);
+  newTree.value = value;
+  newTree.left = undefined;
+  newTree.right = undefined;
+
+  return newTree;
 };
 
-//START
+var bstMethods = {};
+
+bstMethods.insert = function(value){
+  var newBSTree = BinarySearchTree(value);
+  var rootValue = this.value;
+
+  // Change to ternary operators later
+  if(value < rootValue) {
+    if(this.left === undefined) {
+      this.left = newBSTree;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if(this.right === undefined) {
+      this.right = newBSTree;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+bstMethods.contains = function(){};
+
+bstMethods.depthFirstLog = function(){};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
