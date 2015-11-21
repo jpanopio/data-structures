e01ee8f3e5567178feb230a62390de3dfc8cfec0
jpanopio <jcpanopio1@gmail.com>
2015-11-21 15:57:40 -0800

    Correctly implement a doubly linked list

--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -5,15 +5,52 @@ var DoublyLinkedList = function(){
   dLL.head = null;
   dLL.tail = null;
 
-  dLL.addToTail = function(value) {};
+  dLL.addToTail = function(value) {
+    var newNode = new Node(value);
+    if(this.head === null){
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.previous = this.tail;
+      this.tail = newNode;
+    }
+  };
 
-  dLL.removeHead = function() {};
+  dLL.removeHead = function() {
+    var removed = this.head.value;
+    this.head = this.head.next;
+    if(this.head !== null) {
+      this.head.previous = null;
+    }
+    return removed;
+  };
 
-  dLL.contains = function(target) {};
+  dLL.contains = function(target) {
+    var current = this.head;
+    while(current){
+      if(current.value === target){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  };
 
-  dLL.addToHead = function(value) {};
 
-  dLL.removeTail = function() {};
+  dLL.addToHead = function(value) {
+    var newNode = new Node(value);
+    newNode.next = this.head;
+    this.head.previous = newNode;
+    this.head = newNode;
+  };
+
+  dLL.removeTail = function() {
+    var removed = this.tail.value;
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+    return removed;
+  };
 
   return dLL;
 }
