9e7049ba0841dd3b91209d6d3d7fa8d7e5615a01
Julien Panopio <jcpanopio1@gmail.com>
2016-03-09 15:25:40 -0800

    Re-implement Queue with prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,30 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   var newQueue = Object.create(queueMethods);
   newQueue.storage = {};
-  newQueue.queueSize = 0;
+  newQueue.storageSize = 0;
 
   return newQueue;
 };
 
 var queueMethods = {};
 
-queueMethods.size = function(){
-  return this.queueSize >= 0 ? this.queueSize : 0;
-}
+queueMethods.size = function () {
+  return this.storageSize;
+};
 
-queueMethods.enqueue = function(value){
-  this.queueSize++;
-  this.storage[this.queueSize] = value;
-}
+queueMethods.enqueue = function (value) {
+  this.storageSize++;
+  this.storage[this.size()] = value;
+};
 
-queueMethods.dequeue = function(){
-  var dequeued = this.storage[1];
-  for(var key in this.storage){
-    this.storage[key] = this.storage[Number(key) + 1];
+queueMethods.dequeue = function () {
+  dequeued = this.storage[1];
+  delete this.storage[1];
+  this.storageSize === 0 ? this.storageSize : this.storageSize--;
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+    delete this.storage[key];
   }
-  delete this.storage[this.queueSize];
-  this.queueSize--;
+
   return dequeued;
-}
+};
